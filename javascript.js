d3df2edee8c1671c4963e9ff2e86671fb2f9c7ee
Unknown <martinp1@live.se>
2018-09-03 14:51:00 +0200

    Random js initiated

---
+++ javascript.js
@@ -0,0 +1,17 @@
+var quotes = [
+	'Get high or die trying'
+	'She was tickled pink by the good news'
+	'Rise and shine'
+	'Close, but no cigar'
+	'I was just pulling your leg'
+	'Keep your chin up'
+	'I will clean my room when pigs fly'
+	'Hold your horses'
+	'I could play outside till the cows come home'
+	'We\'re all in the same boat'
+]
+
+function newQuote() {
+	var randomNumber = Math.floor(Math.random() * (quotes.length));
+	document.getElementById('quoteDispay').innerHTML = quotes[randomNumber];
+}
